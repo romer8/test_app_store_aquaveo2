e1c4a2aa2e2746a6e9e13725a1ae56408c1c7e35
romer8 <gio.busrom@gmail.com>
2023-01-16 13:04:35 -0700

    add url

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     keywords='',
     author='',
     author_email='',
-    url='',
+    url='https://github.com/romer8/test_app_store_aquaveo2',
     license='',
     packages=find_namespace_packages(),
     package_data={'': resource_files},
