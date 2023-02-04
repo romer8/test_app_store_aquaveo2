f13500c3a730de48364242ad45ea2291fb3e67de
romer8 <gio.busrom@gmail.com>
2023-02-03 21:09:37 -0700

    more tests

--- tethysapp/test_app_store_aquaveo/app.py
+++ tethysapp/test_app_store_aquaveo/app.py
@@ -1,4 +1,5 @@
 from tethys_sdk.base import TethysAppBase
+from tethys_sdk.app_settings import CustomSimpleSetting,CustomSecretSetting,CustomJSONSetting
 
 
 class TestAppStoreAquaveo(TethysAppBase):
@@ -16,3 +17,44 @@ class TestAppStoreAquaveo(TethysAppBase):
     tags = ''
     enable_feedback = False
     feedback_emails = []
+
+
+    def custom_settings(self):
+        custom_settings = (
+
+            CustomSimpleSetting(
+                name='Views_Names',
+                type = CustomSimpleSetting.TYPE_STRING,
+                description='Name of the region holding the views (e.g. La Plata Basin)',
+                required=False,
+                # default="HOLA"
+            ),
+            CustomJSONSetting(
+                name='JSON_Test',
+                description='This is JSON name',
+                required=False,
+                # default={"hola":"bebe"}
+            ),
+            CustomSecretSetting(
+                name='Secret_Test',
+                description='This is SECRET',
+                required=True            
+            ),
+            CustomSecretSetting(
+                name='Secret_Test2',
+                description='This is SECRET',
+                required=True
+            ),
+            CustomSecretSetting(
+                name='Secret_Test3',
+                description='This is SECRET',
+                required=True
+            ),
+            CustomSecretSetting(
+                name='Secret_Test4',
+                description='This is SECRET',
+                required=True
+            ),
+
+        )
+        return custom_settings
--- tethysapp/test_app_store_aquaveo/controllers.py
+++ tethysapp/test_app_store_aquaveo/controllers.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from tethys_sdk.routing import controller
 from tethys_sdk.gizmos import Button
 
+from .app import TestAppStoreAquaveo as app
+import json
+
 @controller
 def home(request):
     """
     Controller for the app home page.
     """
+
+    normal_boy = app.get_custom_setting('Views_Names')
+    json_boy = app.get_custom_setting('JSON_Test')
+
+    secret_boy = app.get_custom_setting('Secret_Test')
+    secret_boy1 = app.get_custom_setting('Secret_Test2')
+    secret_boy2 = app.get_custom_setting('Secret_Test3')
+    secret_boy3 = app.get_custom_setting('Secret_Test4')
+
+    # json_dict =  json.loads(json_boy)
+
+    # print(json_boy)
+    print(normal_boy)
+    print(json_boy)
+    print(secret_boy)
+    print(secret_boy1)
+    print(secret_boy2)
+    print(secret_boy3)
+
     save_button = Button(
         display_text='',
         name='save-button',
@@ -68,7 +90,13 @@ def home(request):
         'edit_button': edit_button,
         'remove_button': remove_button,
         'previous_button': previous_button,
-        'next_button': next_button
+        'next_button': next_button,
+        'normal_setting':normal_boy,
+        'json_setting':json_boy,
+        'secret1_setting':secret_boy,
+        'secret2_setting':secret_boy1,
+        'secret3_setting':secret_boy2,
+        'secret4_setting':secret_boy3,
     }
 
     return render(request, 'test_app_store_aquaveo/home.html', context)
